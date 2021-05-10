eee80bdbc14d424210588dddda3e217fd9758f92
qfg <qiufg@elinkbait.com>
2021-05-10 15:29:18 +0800

    测试提交

---
+++ src/main/java/com/githubtest/test/TestGitHub.java
@@ -0,0 +1,15 @@
+package com.githubtest.test;
+
+import org.junit.jupiter.api.Test;
+
+public class TestGitHub {
+
+    @Test
+    public void test(){
+        System.out.println("testgithub");
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+}
