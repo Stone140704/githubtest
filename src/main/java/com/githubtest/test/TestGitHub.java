4f50bf6ddc3aa4b25dd46bc0781d0730c4eb5662
qfg <qiufg@elinkbait.com>
2021-05-13 16:19:54 +0800

    测试提交3--补充说明

--- src/main/java/com/githubtest/test/TestGitHub.java
+++ src/main/java/com/githubtest/test/TestGitHub.java
@@ -13,7 +13,9 @@ public class TestGitHub {
         logger.info("info test...");
 
         logger.warn("commit cancel...");
-        logger.warn("commit cancel...补充");//amend:修改到上次的提交中(即此次提交叠加到上次提交中,包括说明.但不能消减之前提交的文件.要消减只能reset重来)
+        logger.warn("commit cancel...补充");//amend:修改到上次的提交中(即此次提交叠加到上次提交中,包括说明.但不能消减之前提交的文件.要消减只能reset撤销重来)
+        logger.warn("commit cancel...has pushed");//已推送的,需本地撤销后强制推送一次
+        logger.warn("commit cancel...未 pushed");//本地提交,未推送,只需本地撤销即可
     }
     
     public static void main(String[] args) {
