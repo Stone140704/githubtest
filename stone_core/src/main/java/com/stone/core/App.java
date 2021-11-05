b84d73965715dcb621c84a3166e082b619aa8f47
qfg <qiufg@elinkbait.com>
2021-11-05 18:40:42 +0800

    修改触发构建

--- stone_core/src/main/java/com/stone/core/App.java
+++ stone_core/src/main/java/com/stone/core/App.java
@@ -2,12 +2,10 @@ package com.stone.core;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("修改触发构建!");
+        System.out.println("Hello World!");
     }
 }
