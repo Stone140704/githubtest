b6783796885e87e5061a98e2b700695c950e6b45
qfg <qiufg@elinkbait.com>
2021-11-05 18:46:33 +0800

    修改stone_socket不触发构建

--- stone_socket/src/main/java/com/stone/socket/App.java
+++ stone_socket/src/main/java/com/stone/socket/App.java
@@ -2,12 +2,10 @@ package com.stone.socket;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("修改stone_socket不触发构建!");
+        System.out.println("Hello World!");
     }
 }
